bde991a5ca0ac1d3b0b5cbedd8f5621117466205
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-05-12 20:42:12 +0100

    AI_melee floows player to last knows coords

--- scenes/actors/AI_melee/AI_melee.gd
+++ scenes/actors/AI_melee/AI_melee.gd
@@ -1,15 +1,18 @@
 extends Actor
+#extends KinematicBody2D
 
 export (int) var detect_radius  # size of the visibility circle
 export (float) var fire_rate  # delay time (s) between bullets
 
 var vis_color = Color(.867, .91, .247, 0.1)
-
-var target  # who are we shooting at?
+var hit_pos
+var target
+var t_last_known 
+var velocity 
 
 func _ready():
 	# dim the sprite when not active
-	$Sprite.self_modulate = Color(0.2, 0, 0)
+
 	# set the collision area's radius
 	var shape = CircleShape2D.new()
 	shape.radius = detect_radius
@@ -19,34 +22,51 @@ func _ready():
 
 func _physics_process(delta):
 	update()
-	# if there's a target, rotate towards it and fire
+	# if there's a target, rotate towards it and fire	
 	if target:
-		rotation = (target.position - position).angle()
-		attack(target.position)
+		if is_in_LOS(target.position):
+			t_last_known = target.position
+			rotation = (target.position - position).angle()
+			attack(target.position)
+	elif t_last_known:
+		move_to_global_pos(t_last_known)
+		if (t_last_known-position).length() <= 0.5:
+			t_last_known = null
+		
 
 func attack(pos):
 	#run for the player 
-	print("attack")
-	
-	apply_direction(target.get_global_position() - position)
-	pass
+	move_to_global_pos(target.position)
 
 func _draw():
 	# display the visibility area
 	draw_circle(Vector2(), detect_radius, vis_color)
 
+func is_in_LOS(body) -> bool:
+	# This function checks the line of sight
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(position, body, 
+											[ self ], collision_mask)
+	
+	if result:
+		hit_pos = result.position
+		if result.collider.name == "Player":
+			return true
+	
+	return false
+
 func _on_Visibility_body_entered(body):
-	print("entered")
 	# connect this to the "body_entered" signal
 	if target:
 		return
+	# Need to check if there is a clear line of sight 
 	target = body
-	$Sprite.self_modulate.r = 1.0
+	
 
 func _on_Visibility_body_exited(body):
 	# connect this to the "body_exited" signal
 	if body == target:
 		target = null
-		$Sprite.self_modulate.r = 0.2
+
 
 
