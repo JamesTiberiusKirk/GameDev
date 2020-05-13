ae6984c67e3c25c0985dbf60d93522cc4b26b1ab
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-05-13 14:27:39 +0100

    Fixed bug with following to last known

--- scenes/actors/AI_melee/AI_melee.gd
+++ scenes/actors/AI_melee/AI_melee.gd
@@ -1,45 +1,45 @@
 extends Actor
-#extends KinematicBody2D
 
-export (int) var detect_radius  # size of the visibility circle
-export (float) var fire_rate  # delay time (s) between bullets
+export (int) var detect_radius
+export (float) var attack_delay
 
 var vis_color = Color(.867, .91, .247, 0.1)
 var hit_pos
 var target
 var t_last_known 
 var velocity 
+var can_attack = false
+
+onready var attack_timer = $AttackTimer
 
 func _ready():
-	# dim the sprite when not active
-
-	# set the collision area's radius
 	var shape = CircleShape2D.new()
 	shape.radius = detect_radius
 	$Visibility/CollisionShape2D.shape = shape
 	max_speed = 100
-	
+	attack_timer.wait_time = attack_delay
 
 func _physics_process(delta):
 	update()
-	# if there's a target, rotate towards it and fire	
+	# if there's a target, rotate towards it and attack
 	if target:
 		if is_in_LOS(target.position):
 			t_last_known = target.position
 			rotation = (target.position - position).angle()
 			attack(target.position)
-	elif t_last_known:
-		move_to_global_pos(t_last_known)
+		else:
+			can_attack = false
+	if t_last_known:
 		if (t_last_known-position).length() <= 0.5:
 			t_last_known = null
-		
+		else:
+			move_to_global_pos(t_last_known)
 
 func attack(pos):
-	#run for the player 
 	move_to_global_pos(target.position)
 
 func _draw():
-	# display the visibility area
+	# Visibility area
 	draw_circle(Vector2(), detect_radius, vis_color)
 
 func is_in_LOS(body) -> bool:
@@ -47,26 +47,22 @@ func is_in_LOS(body) -> bool:
 	var space_state = get_world_2d().direct_space_state
 	var result = space_state.intersect_ray(position, body, 
 											[ self ], collision_mask)
-	
 	if result:
 		hit_pos = result.position
 		if result.collider.name == "Player":
 			return true
-	
 	return false
 
 func _on_Visibility_body_entered(body):
-	# connect this to the "body_entered" signal
 	if target:
 		return
-	# Need to check if there is a clear line of sight 
 	target = body
-	
 
 func _on_Visibility_body_exited(body):
-	# connect this to the "body_exited" signal
-	if body == target:
-		target = null
+	target = null
+
+func _on_AttackTimer_timeout():
+	can_attack = true
 
 
 
