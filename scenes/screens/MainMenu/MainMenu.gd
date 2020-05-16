d91eae26cb90ff729602c22f6ac12d0755df2c6a
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-05-16 20:17:28 +0100

    Code cleanup

--- scenes/screens/MainMenu/MainMenu.gd
+++ scenes/screens/MainMenu/MainMenu.gd
@@ -1,16 +1,10 @@
 extends Control
 
-func _ready():
-	var autoload = get_node("/root/Autoload")
-
-
 func _on_NewGame_pressed():
 	get_tree().change_scene("res://scenes/levels/Level1/Level1.tscn")
 
-
 func _on_ChooseLevel_pressed():
 	get_tree().change_scene("res://scenes/screens/LevelSelect/LevelSeclect.tscn")
-
 
 func _on_Exit_pressed():
 	get_tree().quit()
