ea302c9d7a440e7e2513223b15066df5bc93b214
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-05-16 16:07:54 +0100

    Added autogeneration for buttons for each level

--- scenes/screens/LevelSelect/LevelSeclect.gd
+++ scenes/screens/LevelSelect/LevelSeclect.gd
@@ -1,27 +1,22 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var level_list = Autoload.list_files_in_directory("res://scenes/levels/")
+	var button_list_box = $LevelSelect
+	for level in level_list:
+		var new_button = Button.new()
+		new_button.text = level
+		new_button.flat = true
+		new_button.set_text_align(0)
+		new_button.connect("pressed", self, "_select_level", [level])
+		button_list_box.add_child(new_button)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _select_level(obj):
+	print(obj)
+	get_tree().change_scene("res://scenes/levels/"+obj+"/"+obj+".tscn")
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://scenes/screens/MainMenu/MainMenu.tscn")
 
 func _on_TestLevel_pressed():
 	get_tree().change_scene("res://scenes/levels/Test_level/TestLevel.tscn")
-
-
-
-func _on_Button_pressed():
-	get_tree().change_scene("res://scenes/levels/Level1/Level1.tscn")
